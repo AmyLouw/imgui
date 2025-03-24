bc71f14188fa9e944493411b78380fa7abcd8a6b
AmyLouw <71879442+AmyLouw@users.noreply.github.com>
2025-03-24 23:33:31 +0000

    updated tings

--- imgui.cpp
+++ imgui.cpp
@@ -1076,6 +1076,7 @@ CODE
 // [SECTION] INCLUDES
 //-------------------------------------------------------------------------
 
+#include "pch.h"
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
